900f7f8d17bed36a42a4bf085d025cb268bcdc29
WebApp-Yana-Aviel <98655793+WebApp-Yana-Aviel@users.noreply.github.com>
2022-02-11 10:53:57 +0200

    Update sendMessage.py

--- webrobotapp/sendMessage.py
+++ webrobotapp/sendMessage.py
@@ -1,8 +1,22 @@
 import json
 from twilio.rest import Client
+import sqlite3
+
 conf_path="/home/webrgacv/webrobotapp/app/home/conf/twilio.json"
+
 def send_sms_admin_message(temp):
-    message_temp="Raspberry temperature is very high  > 79 C ....."
+    message_temp="Raspberry temperature is very high  > 70 C ....."
+    conn=sqlite3.connect('produc_app.db',check_same_thread=False)
+    cursor=conn.cursor()
+    SQL="SELECT phone from users where name like 'admin'"
+    cursor.execute(SQL)
+    myRes=cursor.fetchone()[0]
+    conn.close()
+    if (myRes is None):
+        phone='+972549111254'
+    else:
+        phone=myRes
+    print(phone)
     # check to see if the Twilio should be used
     f=open(conf_path)
     data=json.load(f)
@@ -11,18 +25,8 @@ def send_sms_admin_message(temp):
         message = client.messages.create( 
                               from_='+972526987310',  
                               body='Hi, admin! '+message_temp+str(temp)+' C',     
-                              to='972549111254' 
+                              to=str(phone)
                           )
     f.close()
     
     
-def send_sms_user_message(message,user_name):
-    f=open(conf_path)
-    data=json.load(f)
-    if data["use_twilio"]:    
-        client = Client(data["account_sid"],data["auth_token"]) 
-        message = client.messages.create( 
-                              from_='+972526987310',  
-                              body='Hi,! '+user_name+'!' +message ,     
-                              to='972549111254' )
-    f.close()
